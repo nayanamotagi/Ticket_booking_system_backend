3405074d0b7891b2a4ea1c040e26cfb44f3ee5f0
nayanamotagi <nayanamotagi24@gmail.com>
2026-07-22 23:38:04 +0530

    change the url cors in app file

--- src/app.js
+++ src/app.js
@@ -14,9 +14,10 @@ const app = express()
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:4173',
-    process.env.FRONTEND_URL || 'https://your-frontend.vercel.app',
-    process.env.ADMIN_FRONTEND_URL || 'https://your-admin-frontend.vercel.app',
+    process.env.FRONTEND_URL || 'https://ticket-booking-system-frontend-5z7pjqw9k-nayanamotagis-projects.vercel.app',
+    process.env.ADMIN_FRONTEND_URL || 'https://ticket-booking-admin-frontend-5ebqc472x-nayanamotagis-projects.vercel.app',
     'https://ticket-booking-system-frontend-5z7pjqw9k-nayanamotagis-projects.vercel.app',
+    'https://ticket-booking-admin-frontend-5ebqc472x-nayanamotagis-projects.vercel.app',
 ]
 
 const corsOptions = {
